bdbcbaeb8c7326c1437fc2a271b2af360530e51f
Ldi569321 <dragon569321@naver.com>
2022-06-02 23:01:07 +0900

    0602_23:01

--- app.js
+++ app.js
@@ -38,3 +38,71 @@ function menuOut() {
 
 menuin.addEventListener("click", menuIn);
 menuout.addEventListener("click", menuOut);
+
+//슬라이더 구간
+let slider = document.querySelector(".slider")
+let banner1 = document.querySelector("#banner1");
+let innerSlider = document.querySelector(".slider-inner")
+let pressed = false
+let startx
+let x
+
+slider.addEventListener("mousedown", e => {
+  pressed = true
+  startx = e.offsetX - innerSlider.offsetLeft
+  slider.style.cursor = "pointer"
+})
+
+slider.addEventListener("mouseenter", () => {
+  slider.style.cursor = "pointer"
+})
+
+slider.addEventListener("mouseup", () => {
+  slider.style.cursor = "pointer"
+})
+
+window.addEventListener("mouseup", () => {
+  pressed = false
+})
+
+slider.addEventListener("mousemove", e => {
+  if (!pressed) return
+  e.preventDefault()
+  x = e.offsetX
+
+  innerSlider.style.left = `${x - startx}px`
+  checkboundary()
+})
+
+function checkboundary() {
+  let outer = slider.getBoundingClientRect()
+  let inner = innerSlider.getBoundingClientRect()
+
+  if (parseInt(innerSlider.style.left) > 0) {
+    innerSlider.style.left = "-0px"
+  } else if (inner.right < outer.right) {
+    innerSlider.style.left = `-${inner.width - outer.width}px`
+  }
+}
+
+const rightClick = document.getElementById("btnRight");
+const leftClick = document.getElementById("btnLeft");
+
+let plus = 0;
+let rightC = false;
+function onclickRight() {
+  if (plus < 3000) {
+    plus += 300;
+    innerSlider.style.left = `-${plus}px`;
+    anima
+  }
+};
+function onclickLeft() {
+  if (plus > 0) {
+    plus -= 300;
+    innerSlider.style.left = `-${plus}px`;
+  }
+};
+
+rightClick.addEventListener("click", onclickRight);
+leftClick.addEventListener("click", onclickLeft);
